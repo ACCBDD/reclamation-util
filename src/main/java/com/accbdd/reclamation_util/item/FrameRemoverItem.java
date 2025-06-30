5efc12a0639823f0b0d85309bfd00239eb04a78e
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-06-30 14:13:21 -0700

    break portal blocks with frame remover as well

--- src/main/java/com/accbdd/reclamation_util/item/FrameRemoverItem.java
+++ src/main/java/com/accbdd/reclamation_util/item/FrameRemoverItem.java
@@ -36,12 +36,12 @@ public class FrameRemoverItem extends Item {
         Level level = pContext.getLevel();
         BlockPos clickedPos = pContext.getClickedPos();
         BlockState state = level.getBlockState(clickedPos);
-        if (state.is(Blocks.END_PORTAL_FRAME)) {
+        if (state.is(Blocks.END_PORTAL_FRAME) || state.is(Blocks.END_PORTAL)) {
             if (!level.isClientSide() && level instanceof ServerLevel serverLevel) {
                 serverLevel.setBlock(clickedPos, Blocks.AIR.defaultBlockState(), 3);
                 serverLevel.playSound(null, clickedPos, SoundEvents.GLASS_BREAK, SoundSource.BLOCKS);
                 Vec3 center = clickedPos.getCenter();
-                serverLevel.sendParticles(new BlockParticleOption(ParticleTypes.BLOCK, Blocks.END_PORTAL_FRAME.defaultBlockState()), center.x, center.y, center.z, 30, 0.25, 0.25, 0.25, 0);
+                serverLevel.sendParticles(new BlockParticleOption(ParticleTypes.BLOCK, state), center.x, center.y, center.z, 30, 0.25, 0.25, 0.25, 0);
             }
             return InteractionResult.sidedSuccess(level.isClientSide);
         }
