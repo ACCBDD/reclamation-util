9a01f958fceb448d86005ea0b2644f06bb9c08b9
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-07-24 04:03:38 -0700

    add rarity to attuned biome essence bottles

--- src/main/java/com/accbdd/reclamation_util/item/EmptyAttunedBiomeBottle.java
+++ src/main/java/com/accbdd/reclamation_util/item/EmptyAttunedBiomeBottle.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
@@ -19,7 +20,8 @@ import net.minecraft.world.level.biome.Biomes;
 public class EmptyAttunedBiomeBottle extends FoilItem {
     public EmptyAttunedBiomeBottle() {
         super(new Properties()
-                .stacksTo(64));
+                .stacksTo(64)
+                .rarity(Rarity.UNCOMMON));
     }
 
     @Override
