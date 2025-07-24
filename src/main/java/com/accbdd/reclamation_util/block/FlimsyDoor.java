6701bca0462ed6642edc6f34eb3c4e7271228292
ACCBDD <9992251+ACCBDD@users.noreply.github.com>
2025-07-24 03:48:53 -0700

    clean code

--- src/main/java/com/accbdd/reclamation_util/block/FlimsyDoor.java
+++ src/main/java/com/accbdd/reclamation_util/block/FlimsyDoor.java
@@ -1,12 +1,10 @@
 package com.accbdd.reclamation_util.block;
 
-import com.accbdd.reclamation_util.register.Items;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
